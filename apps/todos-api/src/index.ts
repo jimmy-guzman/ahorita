e21df5743383e8dafc89ff6402b3c64f2c26f669
jimmy-guzman <hi@jimmyguzman.com>
2024-01-08 20:03:20 -0600

    feat: ✨ colorize api's initial logs

--- apps/todos-api/src/index.ts
+++ apps/todos-api/src/index.ts
@@ -1,18 +1,22 @@
 import cors from '@elysiajs/cors';
 import swagger from '@elysiajs/swagger';
 import { Elysia } from 'elysia';
+import { cyan } from 'picocolors';
 
 import { tags } from './routes/tags';
 import { tasks } from './routes/tasks';
 
 const app = new Elysia()
   .use(cors())
+  .use(tasks)
+  .use(tags)
   .use(
     swagger({
+      path: '/docs',
       documentation: {
         info: { title: 'Ahorita API Docs', version: '', description: '' },
       },
-      exclude: ['/'],
+      exclude: ['/docs', '/docs/json'],
     })
   )
   .onError(({ code, error, set }) => {
@@ -22,10 +26,8 @@ const app = new Elysia()
       return { status: error.status, code, message: error.message };
     }
   })
-  .use(tasks)
-  .use(tags)
   .listen(3000, ({ hostname, port }) => {
-    console.log(`🚀 Running at http://${hostname}:${port}/swagger`);
+    console.log(`🚀 Running at ${cyan(`http://${hostname}:${port}/docs`)}`);
   });
 
 export type App = typeof app;
