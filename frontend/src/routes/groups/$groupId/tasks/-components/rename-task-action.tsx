4f2229b3ec7f2a016481a969e527b37527b02546
jimmy-guzman <hi@jimmyguzman.com>
2024-03-11 21:33:45 -0500

    refactor: ♻️ just use `daisyui` modal + `react`

--- frontend/src/routes/groups/$groupId/tasks/-components/rename-task-action.tsx
+++ frontend/src/routes/groups/$groupId/tasks/-components/rename-task-action.tsx
@@ -1,13 +1,13 @@
 import { typeboxResolver } from "@hookform/resolvers/typebox";
-import * as Dialog from "@radix-ui/react-dialog";
 import { type Static, Type } from "@sinclair/typebox";
 import { useMutation } from "@tanstack/react-query";
 import { PencilIcon, SaveIcon, XIcon } from "lucide-react";
+import { useRef } from "react";
+import { createPortal } from "react-dom";
 import { useForm } from "react-hook-form";
 
 import { editTaskMutationOptions } from "@/api/edit-task";
 import { TextInput } from "@/components/text-input";
-import { useState } from "react";
 
 const schema = Type.Object({
   name: Type.String({ minLength: 1 }),
@@ -20,61 +20,63 @@ interface RenameTaskDialogProps {
 
 export const RenameTaskAction = ({ name, id }: RenameTaskDialogProps) => {
   const editMutation = useMutation(editTaskMutationOptions);
-  const [isDialogOpen, setIsDialogOpen] = useState(false);
-
+  const dialogRef = useRef<HTMLDialogElement>(null);
   const form = useForm<Static<typeof schema>>({
     resolver: typeboxResolver(schema),
     values: { name },
   });
 
   return (
-    <Dialog.Root open={isDialogOpen}>
-      <Dialog.Trigger asChild>
-        <button type="button" onClick={() => setIsDialogOpen(true)}>
-          Rename
-          <span className="dsy-badge dsy-badge-ghost">
-            <PencilIcon className="h-4 w-4" />
-          </span>
-        </button>
-      </Dialog.Trigger>
+    <>
+      <button type="button" onClick={() => dialogRef.current?.showModal()}>
+        Rename
+        <span className="dsy-badge dsy-badge-ghost">
+          <PencilIcon className="h-4 w-4" />
+        </span>
+      </button>
+      {createPortal(
+        <dialog className="dsy-modal" ref={dialogRef}>
+          <div className="dsy-modal-box">
+            <form method="dialog">
+              <button
+                type="submit"
+                className="dsy-btn dsy-btn-sm dsy-btn-circle dsy-btn-ghost absolute top-2 right-2"
+                aria-label="Close"
+              >
+                <XIcon />
+              </button>
+            </form>
 
-      <Dialog.Portal>
-        <Dialog.Overlay />
-        <Dialog.Content asChild onEscapeKeyDown={() => setIsDialogOpen(false)}>
-          <div className="dsy-modal dsy-modal-open">
-            <div className="dsy-modal-box">
-              <Dialog.Close asChild>
+            <h3 className="font-bold text-lg">Rename The Task</h3>
+            <form
+              onSubmit={form.handleSubmit((values) => {
+                editMutation.mutate(
+                  { params: { id }, body: values },
+                  { onSuccess: () => dialogRef.current?.close() },
+                );
+              })}
+            >
+              <TextInput control={form.control} name="name" label="Name" />
+              <div className="dsy-modal-action">
                 <button
                   type="button"
-                  className="dsy-btn dsy-btn-sm dsy-btn-circle dsy-btn-ghost absolute top-2 right-2"
-                  aria-label="Close"
-                  onClick={() => setIsDialogOpen(false)}
+                  className="dsy-btn dsy-btn-outline"
+                  onClick={() => dialogRef.current?.close()}
                 >
-                  <XIcon />
+                  Cancel
                 </button>
-              </Dialog.Close>
-              <h3 className="font-bold text-lg">Rename The Task</h3>
-              <form
-                onSubmit={form.handleSubmit((values) => {
-                  editMutation.mutate(
-                    { params: { id }, body: values },
-                    { onSuccess: () => setIsDialogOpen(false) },
-                  );
-                })}
-              >
-                <TextInput control={form.control} name="name" label="Name" />
-                <div className="dsy-modal-action">
-                  <div>
-                    <button type="submit" className="dsy-btn dsy-btn-neutral">
-                      Save <SaveIcon />
-                    </button>
-                  </div>
-                </div>
-              </form>
-            </div>
+                <button type="submit" className="dsy-btn dsy-btn-neutral">
+                  Save <SaveIcon />
+                </button>
+              </div>
+            </form>
           </div>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+          <form method="dialog" className="dsy-modal-backdrop">
+            <button type="submit">close</button>
+          </form>
+        </dialog>,
+        document.body,
+      )}
+    </>
   );
 };
