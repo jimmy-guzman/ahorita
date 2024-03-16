9ccc82c71af4d3868d91a363c56540dfc0c1542e
jimmy-guzman <hi@jimmyguzman.com>
2024-03-16 15:41:57 -0500

    fix: 🐛 always send request method as uppercase

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -5,6 +5,13 @@ export const api = treaty<App>(import.meta.env.VITE_SERVER_DOMAIN, {
   fetch: {
     credentials: "include",
   },
+  // TODO: change back to patch when https://github.com/elysiajs/eden/issues/61 is addressed
+  fetcher: (requestInfo, requestInit) => {
+    return fetch(requestInfo, {
+      ...requestInit,
+      method: requestInit?.method?.toUpperCase(),
+    });
+  },
 });
 
 export type API = App["_routes"];
