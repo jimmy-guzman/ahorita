86bfaa1e6e1197a0424238ba39423e6c3f24af86
jimmy-guzman <hi@jimmyguzman.com>
2024-06-05 20:06:36 -0500

    feat: ✨ add tooltip to project's actions

--- backend/src/utils.ts
+++ backend/src/utils.ts
@@ -1,6 +1,6 @@
 import { customAlphabet } from "nanoid";
 
-export const nowAsISO = () => new Date().toISOString();
+export const now = () => new Date().toISOString();
 
 // cspell:disable-next-line
 export const nanoid = customAlphabet("6T9834PX7JACKVERYMNDBUHWLFGQ", 4);
--- frontend/src/components/project-details.tsx
+++ frontend/src/components/project-details.tsx
@@ -1,27 +1,20 @@
-import { useMutation, useSuspenseQuery } from "@tanstack/react-query";
+import { useSuspenseQuery } from "@tanstack/react-query";
 import { Outlet } from "@tanstack/react-router";
 import { Link } from "@tanstack/react-router";
 import { getRouteApi } from "@tanstack/react-router";
-import {
-  FolderCheckIcon,
-  FolderIcon,
-  ListTodoIcon,
-  StarIcon,
-  StarOffIcon,
-} from "lucide-react";
+import { ListTodoIcon } from "lucide-react";
 
-import { editProjectOptions } from "@/api/edit-project";
 import { projectQueryOptions } from "@/api/query-project";
 
 import { DeleteProject } from "./delete-project";
 import { EditProject } from "./edit-project";
+import { ProjectActions } from "./project-actions";
 
 const routeApi = getRouteApi("/projects/$projectId");
 
 export const ProjectDetails = () => {
   const { projectId } = routeApi.useParams();
   const { data: project } = useSuspenseQuery(projectQueryOptions(projectId));
-  const { mutate: editProject } = useMutation(editProjectOptions);
 
   return (
     <section className="flex w-full flex-col gap-4">
@@ -29,69 +22,14 @@ export const ProjectDetails = () => {
         <div className="dsy-card-body">
           <h1 className="dsy-card-title flex justify-between text-2xl sm:text-3xl">
             <span>{project.name}</span>
-            <div>
-              {project.isFavorite ? (
-                <button
-                  type="button"
-                  className="dsy-btn dsy-btn-ghost dsy-btn-circle"
-                  onClick={() =>
-                    editProject({
-                      params: { projectId },
-                      body: { isFavorite: false },
-                    })
-                  }
-                >
-                  <StarIcon />
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  className="dsy-btn dsy-btn-ghost dsy-btn-circle"
-                  onClick={() =>
-                    editProject({
-                      params: { projectId },
-                      body: { isFavorite: true },
-                    })
-                  }
-                >
-                  <StarOffIcon />
-                </button>
-              )}
-              {project.isDone ? (
-                <button
-                  type="button"
-                  className="dsy-btn dsy-btn-ghost dsy-btn-circle"
-                  onClick={() =>
-                    editProject({
-                      params: { projectId },
-                      body: { isDone: false },
-                    })
-                  }
-                >
-                  <FolderCheckIcon />
-                </button>
-              ) : (
-                <button
-                  type="button"
-                  className="dsy-btn dsy-btn-ghost dsy-btn-circle"
-                  onClick={() =>
-                    editProject({
-                      params: { projectId },
-                      body: { isDone: true },
-                    })
-                  }
-                >
-                  <FolderIcon />
-                </button>
-              )}
-            </div>
+            <ProjectActions />
           </h1>
           <p>{project.description}</p>
           <div className="dsy-card-actions justify-end">
             <DeleteProject />
             <EditProject />
             <Link
-              className="dsy-btn dsy-btn-primary dsy-btn-sm"
+              className="dsy-btn dsy-btn-primary"
               to="/projects/$projectId/tasks"
               params={{ projectId }}
               activeOptions={{ exact: true }}
