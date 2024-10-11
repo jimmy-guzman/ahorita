a2b19f072cca51149900b7cc2dffe7c4ec9fb7a4
jimmy-guzman <hi@jimmyguzman.com>
2024-10-11 18:49:10 -0500

    test: ✅ setup `msw` & support route apis

--- frontend/src/testing/setup.ts
+++ frontend/src/testing/setup.ts
@@ -1,3 +1,17 @@
 import "@testing-library/jest-dom";
 
+import { server } from "./mocks/server";
+
 vi.mock("zustand");
+
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "warn" });
+});
+
+afterEach(() => {
+  server.resetHandlers();
+});
+
+afterAll(() => {
+  server.close();
+});
