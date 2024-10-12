ed01103238002fc57eaa3aede03e8dda37ae137f
Jimmy Guzman <30631540+jimmy-guzman@users.noreply.github.com>
2024-10-12 18:16:01 -0500

    test: ✅ bring back e2e smoke tests (#168)

--- frontend/e2e/app.spec.ts
+++ frontend/e2e/app.spec.ts
@@ -1,12 +1,4 @@
-import AxeBuilder from "@axe-core/playwright";
-import { faker } from "@faker-js/faker";
 import { expect, test } from "@playwright/test";
-
-const getAxeViolations = async (page: AxeBuilder["page"]) => {
-  const { violations } = await new AxeBuilder({ page }).analyze();
-
-  return violations;
-};
 
 test("should have title", async ({ page }) => {
   await page.goto("/");
@@ -31,92 +23,5 @@ test("should navigate to api docs", async ({ page, context }) => {
 
   await expect(
     newPage.getByRole("heading", { name: "Ahorita API Docs", level: 1 }),
-  ).toBeVisible();
-});
-
-test("should create tag and task", async ({ page }) => {
-  const randomTag = faker.word.noun();
-  const randomTagDescription = faker.word.words();
-  const randomTask = `${faker.word.verb()} ${faker.word.noun()}`;
-
-  await page.goto("/");
-
-  expect(await getAxeViolations(page)).toEqual([]);
-
-  await page.getByRole("link", { name: "Get Started" }).click();
-
-  await expect(
-    page.getByRole("heading", { name: "Add Your New Tag", level: 1 }),
-  ).toBeVisible();
-
-  expect(await getAxeViolations(page)).toEqual([]);
-
-  await page.getByRole("textbox", { name: "Your Tag's Name?" }).fill(randomTag);
-  await page
-    .getByRole("textbox", { name: "Your Tag's Description?" })
-    .fill(randomTagDescription);
-
-  await page.getByRole("button", { name: "Add Tag" }).click();
-
-  await expect(
-    page.getByRole("heading", { name: randomTag, level: 1 }),
-  ).toBeVisible();
-
-  expect(await getAxeViolations(page)).toEqual([]);
-
-  await page.getByRole("button", { name: "Add Task" }).click();
-
-  await expect(
-    page.getByRole("heading", { name: "Add Your New Task", level: 2 }),
-  ).toBeVisible();
-
-  expect(await getAxeViolations(page)).toEqual([]);
-
-  await page
-    .getByRole("textbox", { name: "Your Task's Name?" })
-    .fill(randomTask);
-
-  await page.getByRole("button", { name: "Submit" }).click();
-
-  await page.getByRole("link", { name: "View Tasks" }).click();
-
-  await expect(
-    page.getByRole("heading", { name: "Your Tasks", level: 2 }),
-  ).toBeVisible();
-
-  await expect(page.getByRole("cell", { name: randomTask })).toBeVisible();
-
-  expect(await getAxeViolations(page)).toEqual([]);
-
-  await expect(page.getByRole("cell", { name: "Todo" })).toBeVisible();
-
-  const row = page.getByRole("row", {
-    name: `${randomTask} Todo Medium Open Menu`,
-  });
-
-  await row.getByRole("button", { name: "Open menu" }).click();
-
-  const actions = row.getByRole("list", { name: `${randomTask} actions` });
-
-  await actions.getByText("Status").click();
-
-  await actions.getByRole("button", { name: "Done" }).click();
-
-  await expect(
-    page.getByRole("row", { name: `${randomTask} Done Medium Open Menu` }),
-  ).toBeVisible();
-
-  await page.getByRole("button", { name: `Delete ${randomTask}` }).click();
-
-  await expect(actions).not.toBeVisible();
-
-  await expect(page.getByRole("cell", { name: randomTask })).not.toBeVisible();
-
-  await page.getByRole("button", { name: "Delete Tag" }).click();
-
-  await expect(page.getByRole("link", { name: randomTag })).not.toBeVisible();
-
-  await expect(
-    page.getByRole("heading", { name: "Add Your New Tag", level: 1 }),
   ).toBeVisible();
 });
